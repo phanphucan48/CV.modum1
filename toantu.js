28ed2d129d15ab12b83dfdc83a1ee590dc641991
unknown <phanphucan4890@gmail.com>
2020-06-03 11:33:39 +0700

    hoanthanhthuchanh

---
+++ toantu.js
@@ -0,0 +1,6 @@
+var width = prompt('nhập chiều rộng');
+var height = prompt('nhập  chiều cao');
+var a = parseInt(width);
+var b = parseInt(height);
+var dientich = (a * b);
+document.write ('dien tích '+ dientich);
