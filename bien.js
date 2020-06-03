cecb2efbc67fdab8a3cf531c43b92835b3809637
unknown <phanphucan4890@gmail.com>
2020-06-03 13:37:14 +0700

    hoanthanhbien

---
+++ bien.js
@@ -0,0 +1,5 @@
+var diemVatLy = +prompt("Nhập vào điểm lý" );
+var diemhoahoc = +prompt('Nhập vào điểm hoá')
+var diemsinhhoc = +prompt('Nhập vào điểm sinh')
+var diemtrungbinh = (diemVatLy+diemhoahoc+diemsinhhoc)/3;
+document.write ('diem trung binh =' + diemtrungbinh)
