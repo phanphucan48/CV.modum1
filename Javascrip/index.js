a993699944ea9252187ac38cb7bce9c476666cdd
phanphucan48 <phanphucan4890@gmail.com>
2020-09-04 13:38:47 +0700

    hoan thanh

--- Javascrip/index.js
+++ Javascrip/index.js
@@ -1,3 +1,12 @@
-function Showmessenger() {
-  alert("Xin chào");
+function doitien() {
+  let money = document.getElementById("amount");
+  let area = document.getElementById("area");
+  if (area.value == 1) {
+    document.getElementById("resuft").innerHTML =
+      "resuft : " + money.value / 23000 + "USD";
+  }
+  if (area.value == 2) {
+    document.getElementById("resuft").innerHTML =
+      "resuft : " + money.value * 23000 + "VND";
+  }
 }
