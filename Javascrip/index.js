38dc2416799adf566115f62bed7def43ae49bde5
phanphucan48 <phanphucan4890@gmail.com>
2020-09-07 11:10:28 +0700

    hoan thanh

--- Javascrip/index.js
+++ Javascrip/index.js
@@ -1,12 +1,21 @@
 function doitien() {
   let money = document.getElementById("amount");
   let area = document.getElementById("area");
-  if (area.value == 1) {
+  let menhgia = document.getElementById("denominations");
+
+  if (area.value == 1 && menhgia.value == 3) {
     document.getElementById("resuft").innerHTML =
-      "resuft : " + money.value / 23000 + "USD";
+      "resuft : " + money.value / 23000 + " USD";
+  } else if (area.value == 1 && menhgia.value == 4) {
+    document.getElementById("resuft").innerHTML =
+      "resuft : " + money.value * 1 + " VND";
   }
-  if (area.value == 2) {
+
+  if (area.value == 2 && menhgia.value == 4) {
     document.getElementById("resuft").innerHTML =
-      "resuft : " + money.value * 23000 + "VND";
+      "resuft : " + money.value * 23000 + " VND";
+  } else if (area.value == 2 && menhgia.value == 3) {
+    document.getElementById("resuft").innerHTML =
+      "resuft : " + money.value * 1 + " USD";
   }
 }
